6e5d3239a211faa71469fcacb059c3f8be81bc61
miqdad-aad <aadfahmi@gmail.com>
2022-01-20 21:54:40 +0700

    oi

---
+++ src/components/header.jsx
@@ -0,0 +1,50 @@
+
+
+function Header() {
+    return (
+      <div className="Header">
+      <header>
+  </header>
+  {/* header */}
+  {/* header navigation */}
+  <nav className="border-bottom nav-ds">
+    <div className="navigation-wrapper container flex-space-between align-items-center">
+      <div className="nav-left-hand-part">
+        <div className="logo">
+          <a className="logo-link" href="01-home-ds.html">
+            <div className="logo-img"><img src={require('../assets/svg/logo-ds.svg').default} alt="logo" /></div>
+            <div className="logo-text">Cryptoki</div>
+          </a>
+        </div>
+        
+      <div className="searchbox">
+  <form method="get" className="main-search">
+    <input className="main-search-params" type="hidden" name="params" defaultValue="all" />
+    <input type="text" className="search-input" placeholder="Enter your search here..." />
+    <button className="search-button">
+      <svg className="crumina-icon">
+        <use xlinkHref="#search-icon" />
+      </svg>
+    </button>
+  </form>
+</div>
+      </div>
+
+      <div className="nav-right-hand-part">
+        {/* header user profile */}
+        <ul className="navigation-menu">
+          <li className="menu-item"><a className="menu-link" href="01-home-ds.html">Home</a></li>
+        </ul>
+        {/* header user profile */}
+      </div>
+    </div>
+    {/* header search block */}
+    {/* header search block */}
+  </nav>
+
+      </div>
+      );
+    }
+    
+    export default Header;
+    
